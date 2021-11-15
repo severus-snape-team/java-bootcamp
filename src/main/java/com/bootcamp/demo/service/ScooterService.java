e8d09bc96a50a40746fefb083284de2eca1a81ba
octav <octavsteaua11@gmail.com>
2021-11-15 19:09:18 +0200

    Added a validator for the scooter form

--- src/main/java/com/bootcamp/demo/service/ScooterService.java
+++ src/main/java/com/bootcamp/demo/service/ScooterService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -21,8 +22,29 @@ public class ScooterService {
         this.scooterRepository = scooterRepository;
     }
 
+    public boolean scooterValidator(Scooter scooter){
+        // ----------- VALOAREA DEFAULT LA STRING-URI IN FIRESTORE ESTE "" NU null   --------------- ///
+        boolean docNameValidator = !scooter.getDocumentName().equals("");
+        // numai litere mici, mari si numere => regular expression
+        boolean serialNumberValidator = scooter.getSerialNumber().matches("^[0-9a-zA-Z]*$") && !scooter.getSerialNumber().equals("");
+        boolean brandValidator = !scooter.getBrand().equals("");
+        boolean costValidator = scooter.getCost().intValue() > 0; // daca costul este mai mare decat 0
+        boolean prodYearValidator = scooter.getProdYear() > 2000; // asta daca vrem sa avem scootere mai noi de anul 2000
+        boolean weightValidator = scooter.getWeight() > 0.0; // stiu ca nu e bine ca e pe double, dar deocamdata las asa
+        for (State s : State.values()) {   // daca valoarea introdusa la state este in enum atunci o sa fie true deci returnez SI intre ceilalti validatori
+            if (s.equals(scooter.getState())) {
+                return docNameValidator && serialNumberValidator && brandValidator && costValidator && prodYearValidator && weightValidator;
+            }
+        }
+        return false;
+    }
+
     public void insertScooter(Scooter scooter) {
-        this.scooterRepository.insertScooter(scooter);
+        if(scooterValidator(scooter)) {
+            this.scooterRepository.insertScooter(scooter);
+        } else {
+            throw new InputMismatchException();
+        }
     }
 
     public ArrayList<Scooter> returnAllScooters() {
