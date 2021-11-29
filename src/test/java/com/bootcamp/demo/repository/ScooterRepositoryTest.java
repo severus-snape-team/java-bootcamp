624e6d3ebdfdd0a05c03ca28226063d04fc6dfee
917_Zachman_Roxana <zrie3047@scs.ubbcluj.ro>
2021-11-29 12:27:21 +0200

    Added more unit tests

--- src/test/java/com/bootcamp/demo/repository/ScooterRepositoryTest.java
+++ src/test/java/com/bootcamp/demo/repository/ScooterRepositoryTest.java
@@ -1,19 +1,24 @@
 package com.bootcamp.demo.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-
-import com.google.cloud.firestore.Firestore;
+import com.bootcamp.demo.model.Scooter;
+import com.bootcamp.demo.model.State;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class ScooterRepositoryTest {
 
@@ -21,12 +26,37 @@ class ScooterRepositoryTest {
 
     @Mock
     private Firestore firestoreDBMock;
+    @Mock
+    private CollectionReference collectionReferenceMock;
+    @Mock
+    private DocumentReference documentReferenceMock;
+    @Mock
+    private ApiFuture<WriteResult> apiFutureMock;
+    @Mock
+    private List<QueryDocumentSnapshot> queryDocumentSnapshotListMock;
+    @Mock
+    private ApiFuture<QuerySnapshot> querySnapshotApiFutureMock;
+    @Mock
+    private ApiFuture<DocumentSnapshot> documentSnapshotApiFutureMock;
+    @Mock
+    private DocumentSnapshot documentSnapshotMock;
+    @Mock
+    private QuerySnapshot querySnapshotMock;
+    @Mock
+    private QueryDocumentSnapshot queryDocumentSnapshotMock;
+    @Mock
+    private Iterator<QueryDocumentSnapshot> iteratorMock;
+    @Mock
+    private InterruptedException interruptedExceptionMock;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         repository = new ScooterRepository();
         repository.setFirestoreDB(firestoreDBMock);
+
+        when(firestoreDBMock.collection(anyString())).thenReturn(collectionReferenceMock);
+        when(collectionReferenceMock.document(anyString())).thenReturn(documentReferenceMock);
     }
 
     @AfterEach
@@ -35,9 +65,103 @@ class ScooterRepositoryTest {
 
     @Test
     void shouldInsertScooter() {
+        when(collectionReferenceMock.document(anyString())).thenReturn(documentReferenceMock);
+        when(documentReferenceMock.set(any())).thenReturn(apiFutureMock);
+        when(firestoreDBMock.collection(anyString())).thenReturn(collectionReferenceMock);
+        when(firestoreDBMock.collection(anyString()).document(anyString())).thenReturn(documentReferenceMock);
+        when(firestoreDBMock.collection(anyString()).document(anyString()).set(any())).thenReturn(apiFutureMock);
+
+        this.repository.insertScooter(new Scooter("doc1", "serial1", "brand1", "1", 2001, 1.1f, State.IN_USE));
+    }
+
+    @Test
+    void shouldUpdateScooter() {
+        when(collectionReferenceMock.document(anyString())).thenReturn(documentReferenceMock);
+        when(documentReferenceMock.update(anyString(), anyString())).thenReturn(apiFutureMock);
+        when(firestoreDBMock.collection(anyString())).thenReturn(collectionReferenceMock);
+        when(firestoreDBMock.collection(anyString()).document(anyString())).thenReturn(documentReferenceMock);
+        when(firestoreDBMock.collection(anyString()).document(anyString()).update(anyString(), anyString())).thenReturn(apiFutureMock);
+
+        this.repository.updateScooter("doc1", "f1", "v1");
+        assertEquals("Document doc1 had f1 updated with v1", this.repository.updateScooter("doc1", "f1", "v1"));
+
+        this.repository.updateScooter("doc1", "documentName", "v1");
+        assertEquals("Document doc1 had documentName updated with v1", this.repository.updateScooter("doc1", "documentName", "v1"));
+
+        this.repository.updateScooter("doc1", "serialNumber", "v1");
+        assertEquals("Document doc1 had serialNumber updated with v1", this.repository.updateScooter("doc1", "serialNumber", "v1"));
+
+        this.repository.updateScooter("doc1", "brand", "v1");
+        assertEquals("Document doc1 had brand updated with v1", this.repository.updateScooter("doc1", "brand", "v1"));
+
+        this.repository.updateScooter("doc1", "state", "v1");
+        assertEquals("Document doc1 had state updated with v1", this.repository.updateScooter("doc1", "state", "v1"));
+
+        this.repository.updateScooter("doc1", "weight", "1.1");
+        assertEquals("Document doc1 had weight updated with 1.1", this.repository.updateScooter("doc1", "weight", "1.1"));
+
+        this.repository.updateScooter("doc1", "cost", "1");
+        assertEquals("Document doc1 had cost updated with 1", this.repository.updateScooter("doc1", "cost", "1"));
+
+        this.repository.updateScooter("doc1", "prodYear", "2020");
+        assertEquals("Document doc1 had prodYear updated with 2020", this.repository.updateScooter("doc1", "prodYear", "2020"));
     }
 
     @Test
     void shouldDeleteScooter() {
+        when(collectionReferenceMock.document(anyString())).thenReturn(documentReferenceMock);
+        when(documentReferenceMock.delete()).thenReturn(apiFutureMock);
+        when(firestoreDBMock.collection(anyString())).thenReturn(collectionReferenceMock);
+        when(firestoreDBMock.collection(anyString()).document(anyString())).thenReturn(documentReferenceMock);
+        when(firestoreDBMock.collection(anyString()).document(anyString()).delete()).thenReturn(apiFutureMock);
+        this.repository.deleteScooter("doc1");
+
+        assertEquals("Document documentName was deleted", this.repository.deleteScooter("documentName"));
     }
+
+    @Test
+    void shouldReadScooter() throws ExecutionException, InterruptedException {
+        when(firestoreDBMock.collection(anyString())).thenReturn(collectionReferenceMock);
+        when(collectionReferenceMock.get()).thenReturn(querySnapshotApiFutureMock);
+        when(querySnapshotApiFutureMock.get()).thenReturn(querySnapshotMock);
+        when(querySnapshotMock.getDocuments()).thenReturn(queryDocumentSnapshotListMock);
+
+        when(queryDocumentSnapshotListMock.iterator()).thenReturn(iteratorMock);
+        when(iteratorMock.hasNext()).thenReturn(Boolean.TRUE, Boolean.FALSE);
+        when(iteratorMock.next()).thenReturn(queryDocumentSnapshotMock, queryDocumentSnapshotMock);
+
+        when(queryDocumentSnapshotMock.getString(anyString())).thenReturn("IN_USE");
+        when(queryDocumentSnapshotMock.getString("cost")).thenReturn("1");
+        when(queryDocumentSnapshotMock.getLong(anyString())).thenReturn(123L);
+        when(queryDocumentSnapshotMock.getDouble(anyString())).thenReturn(1.1);
+        List<Scooter> scooters = this.repository.readScooters();
+        assertNotNull(scooters);
+        assertEquals(scooters.get(0), new Scooter("IN_USE", "IN_USE", "IN_USE", "1", 123, 1.1, State.IN_USE));
+        verify(firestoreDBMock, times(1)).collection(anyString());
+
+    }
+
+    @Test
+    void shouldGetScooterByName() throws ExecutionException, InterruptedException {
+        when(firestoreDBMock.collection(anyString())).thenReturn(collectionReferenceMock);
+        when(collectionReferenceMock.document(anyString())).thenReturn(documentReferenceMock);
+        when(documentReferenceMock.get()).thenReturn(documentSnapshotApiFutureMock);
+        when(documentSnapshotApiFutureMock.get()).thenReturn(documentSnapshotMock);
+        assertNotNull(this.repository.getScooterByName(""));
+        verify(firestoreDBMock, times(1)).collection(anyString());
+
+    }
+
+    @Test
+    void shouldNotGetScooterByName() throws ExecutionException, InterruptedException {
+        when(firestoreDBMock.collection(anyString())).thenReturn(collectionReferenceMock);
+        when(collectionReferenceMock.document(anyString())).thenReturn(documentReferenceMock);
+        when(documentReferenceMock.get()).thenReturn(documentSnapshotApiFutureMock);
+        when(documentSnapshotApiFutureMock.get()).thenReturn(documentSnapshotMock);
+        when(documentSnapshotApiFutureMock.get()).thenThrow(interruptedExceptionMock);
+        this.repository.getScooterByName("");
+        verify(firestoreDBMock, times(1)).collection(anyString());
+
+    }
+
 }
